f46695175c351b289e8de07a6379ca125d1b989f
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-02-25 13:42:43 -0600

    typo

--- src/components/home/main/AboutMe.jsx
+++ src/components/home/main/AboutMe.jsx
@@ -24,7 +24,7 @@ function AboutMe() {
                     With a Master's degree in Public Administration, I have cultivated a deep understanding of effective communication and leadership. Public speaking isn't just a skill for me—it's a passion. I thrive on the energy of engaging conversations and the opportunity to inspire and empower others through my words.
                 </p>
                 <p>
-                    Beyond the podium, I am a social butterfly at heart. I genuinely enjoy connecting with people from all walks of life, making new friends, and building meaningful relationships. Whether it's networking at events or engaging in lively discussions, Me Kezia-Grace believes in the power of human connection to drive positive change.
+                    Beyond the podium, I am a social butterfly at heart. I genuinely enjoy connecting with people from all walks of life, making new friends, and building meaningful relationships. Whether it's networking at events or engaging in lively discussions, I, Kezia-Grace believes in the power of human connection to drive positive change.
                 </p>
                 <p>
                     When you book me for your event, you can expect not only a seasoned speaker with expertise but also someone who brings warmth, enthusiasm, and authenticity to every interaction.
